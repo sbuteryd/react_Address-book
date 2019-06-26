89bcb66c1a244afcfc6bf4bb5742c431251aa767
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-26 21:30:05 +0800

    搜索和显示搜索结果

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
-
+// escape-string-RegularExpression
+import escapeRegexp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 export default class ListContacts extends  Component{
     state ={
         query:''
@@ -10,14 +12,34 @@ export default class ListContacts extends  Component{
            query:values
        })
     }
+    clearQuery = ()=>{
+        console.log('russell')
+        this.setState({
+            query:''
+        })
+    }
     render() {
+        let showContacts;
+        if(this.state.query){
+            const match = new RegExp(escapeRegexp(this.state.query),'i');
+            showContacts = this.props.contacts.filter(c=> match.test(c.name))
+        }else {
+            showContacts = this.props.contacts
+        }
         return (
             <div>
                 <div>
                     <input value={this.state.query} onChange={(e)=>this.updateSearch(e.target.value)} className='search-contacts' type="text"/>
                 </div>
+                {this.props.contacts.length !== showContacts.length &&(
+                    <div className='showing-contacts'>
+                        <p> Showing:{showContacts.length} total: {this.props.contacts.length} </p>
+                        <button onClick={this.clearQuery}>Show all</button>
+                    </div>
+
+                )}
                 <div className='contact-list'>
-                    {this.props.contacts.map((contact)=>(
+                    {showContacts.map((contact)=>(
                         <li key={contact.id} className='contact-list-item'>
                             <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}}/>
                             <div className='contact-details'>
