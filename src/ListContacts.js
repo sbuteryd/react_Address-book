ce45a1d260038cb10b3afecd33386f608f75df97
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-26 21:05:51 +0800

    更新搜索状态

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react'
 
 export default class ListContacts extends  Component{
+    state ={
+        query:''
+    }
+
+    updateSearch =(values)=>{
+       this.setState({
+           query:values
+       })
+    }
     render() {
-        console.log(this.props.contacts);
         return (
             <div>
                 <div>
+                    <input value={this.state.query} onChange={(e)=>this.updateSearch(e.target.value)} className='search-contacts' type="text"/>
+                </div>
+                <div className='contact-list'>
                     {this.props.contacts.map((contact)=>(
                         <li key={contact.id} className='contact-list-item'>
                             <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}}/>
