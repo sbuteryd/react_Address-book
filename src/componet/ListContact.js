85aeaeaafbdd6c335444cd61c615c608d7159a53
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-27 22:46:48 +0800

    3 输入时更新值

--- src/componet/ListContact.js
+++ src/componet/ListContact.js
@@ -2,12 +2,24 @@ import React, {Component} from 'react'
 
 
 export default class ListContact extends Component{
+    state = {
+        query:''
+    }
     updateContacts =(contact)=>{
         this.props.onDeleteContact(contact)
+    };
+    updateInput = (query)=>{
+        this.setState(()=>({
+            query:query
+        }))
     };
     render() {
         return (
             <div>
+                {JSON.stringify(this.state.query)}
+                <div className='contact-list'>
+                    <input className='search-contacts' value={this.state.query} onChange={(event)=>this.updateInput(event.target.value)} type="text"/>
+                </div>
                 {this.props.contacts.map((contact)=>(
                     <li className='contact-list-item' key={contact.id}>
                         <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}} />
