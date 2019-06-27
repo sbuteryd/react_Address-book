7eb45881c39055c3d73803d3e079a6c395ff8f8b
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-27 22:35:05 +0800

    2 渲染ui

--- src/componet/ListContact.js
+++ src/componet/ListContact.js
@@ -3,10 +3,19 @@ import React, {Component} from 'react'
 
 export default class ListContact extends Component{
     render() {
-        {console.log('hello',this.props.contacts)}
         return (
             <div>
-                hellow
+                {this.props.contacts.map((contact)=>(
+                    <li className='contact-list-item' key={contact.id}>
+                        <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}} />
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                        <button className='contact-remove'>remove</button>
+
+                    </li>
+                ))}
             </div>
         );
     }
