42472c985237ef57a3a09899fb2305b4176643d3
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-27 22:21:48 +0800

    获取数据并验证有数据

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React,{Component} from 'react'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 
 class App extends Component{
+    state ={
+        contactsList:[]
+    };
+    componentDidMount() {
+        ContactsAPI.getAll().then((contacts)=>{
+            this.setState(({contactsList: contacts}))
+        })
+    }
+
     render() {
+        {console.log('state',this.state.contactsList)}
         return(
             <div>
                 Hello
