452b4e1795444b593e361cadd79ea7c92d2001b4
sbuteryd@qq.com <sbuteryd@qq.com>
2019-06-29 17:38:12 +0800

    安装router serialize

---
+++ src/CreaeContact.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import  Route from 'react-router-dom'
+import serialize from 'form-serialize'
+class CreaeContact extends Component{
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
